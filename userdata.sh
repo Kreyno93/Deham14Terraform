236a27beab8076ff09fbfd6e6dbb4884f4940d8b
Kreyno93 <michael.wagener@neuefische.de>
2024-07-05 16:23:19 +0200

    change userscript to use docker with a personal image

--- userdata.sh
+++ userdata.sh
@@ -3,67 +3,15 @@
 # Install updates
 sudo yum update -y
 
-# Install Apache server
-sudo yum install -y httpd
+# Install Docker
+sudo yum install docker -y
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
 
-# Install MariaDB, PHP, and necessary tools
-sudo amazon-linux-extras install -y php8.0
-sudo amazon-linux-extras enable mariadb10.5
-sudo yum clean metadata
-sudo yum install -y mariadb unzip
+# Pull Image
+sudo docker pull miischa/rickandmorty-gallery
 
+# Run Container | In this case I am using a personal image that I have uploaded to Docker Hub
+# Use different Images for different purposes. Like Nginx for Web Server, MySQL for Database, etc.
+sudo docker run -d -p 80:3000 miischa/rickandmorty-gallery
 
-# Set database variables
-DBName="wordpress"
-DBUser="wordpress"
-DBPassword="admin1234"
-DBRootPassword="rootpassword1234"
-DBHost="localhost"
-
-# Start Apache server and enable it on system startup
-sudo systemctl start httpd
-sudo systemctl enable httpd
-
-# Start MariaDB service and enable it on system startup
-sudo systemctl start mariadb
-sudo systemctl enable mariadb
-
-# Wait for MariaDB to fully start
-sleep 10
-
-# Set MariaDB root password
-sudo mysqladmin -u root password "$DBRootPassword"
-
-# Download and install WordPress
-sudo wget http://wordpress.org/latest.tar.gz -P /var/www/html
-cd /var/www/html
-sudo tar -zxvf latest.tar.gz
-sudo cp -rvf wordpress/* .
-sudo rm -R wordpress
-sudo rm latest.tar.gz
-
-# Making changes to the wp-config.php file, setting the DB name
-sudo cp ./wp-config-sample.php ./wp-config.php 
-sudo sed -i "s/'database_name_here'/'$DBName'/g" wp-config.php
-sudo sed -i "s/'username_here'/'$DBUser'/g" wp-config.php
-sudo sed -i "s/'password_here'/'$DBPassword'/g" wp-config.php
-sudo sed -i "s/'localhost'/'$DBHost'/g" wp-config.php
-
-# Grant permissions
-sudo usermod -a -G apache ec2-user
-sudo chown -R ec2-user:apache /var/www
-sudo chmod 2775 /var/www
-sudo find /var/www -type d -exec chmod 2775 {} \;
-sudo find /var/www -type f -exec chmod 0664 {} \;
-
-# Create WordPress database
-echo "CREATE DATABASE IF NOT EXISTS $DBName;" | mysql -u root --password=$DBRootPassword
-echo "CREATE USER IF NOT EXISTS '$DBUser'@'localhost' IDENTIFIED BY '$DBPassword';" | mysql -u root --password=$DBRootPassword
-echo "GRANT ALL ON $DBName.* TO '$DBUser'@'localhost';" | mysql -u root --password=$DBRootPassword
-echo "FLUSH PRIVILEGES;" | mysql -u root --password=$DBRootPassword
-
-# # Create WordPress database - commented out due to version error. had mariadb 5.5 previously
-# echo "CREATE DATABASE \`${DBName}\`;" | mysql -u root --password="$DBRootPassword"
-# echo "CREATE USER '$DBUser'@'localhost' IDENTIFIED BY '$DBPassword';" | mysql -u root --password="$DBRootPassword"
-# echo "GRANT ALL ON \`${DBName}\`.* TO '$DBUser'@'localhost';" | mysql -u root --password="$DBRootPassword"
-# echo "FLUSH PRIVILEGES;" | mysql -u root --password="$DBRootPassword"
